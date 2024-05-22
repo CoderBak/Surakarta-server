e63b14aa687f11f256a63506654d46c966155e56
CoderBak <hx-sun@outlook.com>
2024-05-22 09:38:14 +0800

    Modify log system

--- utils/logger.h
+++ utils/logger.h
@@ -13,15 +13,17 @@ public:
     void save(const QString& title, QTcpSocket* client1, QTcpSocket* client2) {
         qDebug() << "Log title: " << title;
         qDebug() << "Log content: " << log;
-        /*
         qDebug() << "Saving log";
-        QFile file(title + ".log");
+        QDir dir;
+        if (!dir.exists("./log")) {
+            dir.mkpath("./log");
+        }
+        QFile file("./log/" + title + ".log");
         if (file.open(QIODevice::WriteOnly)) {
             QTextStream stream(&file);
             stream << log;
             qDebug() << "Log saved";
         }
-        */
         client1->write(("$L" + title + log).toUtf8());
         client2->write(("$L" + title + log).toUtf8());
         log.clear();
